e2507a58bb0db15544dd1e0e05558acfa868148e
Arpit Bajpai <arpitbajpai@qainfotech.com>
2017-06-27 14:37:40 +0530

    Update PropertiesOptionsIO.java

--- src/main/java/com/qainfotech/tap/training/resourceio/PropertiesOptionsIO.java
+++ src/main/java/com/qainfotech/tap/training/resourceio/PropertiesOptionsIO.java
@@ -1,18 +1,34 @@
 package com.qainfotech.tap.training.resourceio;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Properties;
 
 /**
  *
  * @author Ramandeep RamandeepSingh AT QAInfoTech.com
  */
 public class PropertiesOptionsIO{
-    
-    public Object getOptionValue(String optionKey) throws IOException {
-        throw new UnsupportedOperationException("Not implemented.");
-    }
+	Properties prop = new Properties();
 
-    public void addOption(String optionKey, Object optionValue) throws IOException {
-        throw new UnsupportedOperationException("Not implemented.");
-    }
+	public Object getOptionValue(String optionKey) throws IOException {
+
+	InputStream input = null;
+
+	input = getClass().getClassLoader().getResourceAsStream("options.properties");
+	prop.load(input);
+	Object a=prop.getProperty(optionKey);
+
+	return a;
+	}
+
+	public void addOption(String optionKey, Object optionValue) throws IOException {
+	OutputStream output = null; 
+	output = new FileOutputStream("C:\\Users\\arpitbajpai\\Downloads\\assignment-resource-io-master\\src\\main\\resources\\options.properties");
+	prop.setProperty(optionKey, optionValue.toString());
+    prop.store(output,null);
+	output.close();
+	}
 }
